d5dd8c9b45115b2be026b294a7d5c52955b44e53
dr-str <65511468+dr-str@users.noreply.github.com>
2021-01-28 19:19:10 +0330

    Using List instead of elif
    I'm new to GitHub and don't know if this is the place for suggestions but creating a list makes it simpler than using elif

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,14 @@
 import random
-
-name = "Joe"
-question = "Will I win the lottery?"
-answer = ""
-
-random_number = random.randint(1, 9)
+name = 'David'
+question = 'Is drinking a good habit?'
+answer = ['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.', 'Hmm, maybe', 'wtf, no']
+random_number = random.randint(0, 10)
 # print(random_number)
-
-if random_number == 1:
-  answer = "Yes - definitely"
-elif random_number == 2:
-  answer = "It is decidedly so"
-elif random_number == 3:
-  answer = "Without a doubt"
-elif random_number == 4:
-  answer = "Reply hazy, try again"
-elif random_number == 5:
-  answer = "Ask again later"
-elif random_number == 6:
-  answer = "Better not tell you now"
-elif random_number == 7:
-  answer = "My sources say no"
-elif random_number == 8:
-  answer = "Outlook not so good"
-elif random_number == 9:
-  answer = "Very doubtful"
+if not len(question) == 0:
+  if len(name) == 0:
+    print('Question: ' + question)
+  else:
+    print(name + ' asks: ' + question)
+  print("Magic 8-Ball's answer: " + answer[random_number])
 else:
-  answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+  print("You haven't asked anything.")
